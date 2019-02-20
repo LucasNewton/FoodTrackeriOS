159c54c5fc51c494862172b9c3ff37608d8e12c6
Lucas Newton <ldn15a@acu.edu>
2019-02-20 08:12:10 -0600

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Lucas Newton on 2/20/19.
+//  Copyright © 2019 Lucas Newton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
