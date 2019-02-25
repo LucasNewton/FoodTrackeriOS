edf25eea4a69da701c0b7ece9f96615916cdd0de
Lucas Newton <ldn15a@acu.edu>
2019-02-24 23:24:18 -0600

    Pushing checkpoint

--- FoodTracker/AppDelegate.swift
+++ FoodTracker/AppDelegate.swift
@@ -12,7 +12,6 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    var nothing = 0;
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
